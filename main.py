3b201f54e787427bb7c64a7edfc75634b7649b37
RenguPaul <pavelatch@gmail.com>
2023-04-08 21:09:48 +0300

    ok

--- main.py
+++ main.py
@@ -93,7 +93,6 @@ def login():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
             return redirect("/")
         return render_template('login.html',
                                message="Неправильный логин или пароль",
